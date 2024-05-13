6c0b09213aa7f806645ca2f570f28ec6ac91b11f
Igwe Francis <Igwefran6.dev@gmail.com>
2024-05-13 16:52:03 +0100

    Refactor component names and update GameBoard.jsx
    - Rename Color-Block.jsx to ColorBlock.jsx
    - Rename Color-Palate-View.jsx to ColorPalateView.jsx
    - Rename How-To-Play.jsx to HowToPlay.jsx
    - Rename Power-Ups.jsx to PowerUps.jsx
    - Update references in src/App.jsx and src/components/GameBoard.jsx
    
    This commit improves code readability by refactoring component names according to camelCase convention and updates the GameBoard component for consistency. Note: Moved codes up from GameBoard to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Header from "./components/Header";
 import Fill from "./components/Fill";
 import GameBoard from "./components/GameBoard";
-import ColorPalateView from "./components/Color-Palate-View.jsx";
-import PowerUps from "./components/Power-Ups";
+import ColorPalateView from "./components/ColorPalateView"; // Fixed import statement
+import PowerUps from "./components/PowerUps";
 import Footer from "./components/Footer";
 import Data from "./components/Data";
 import Win from "./components/Win";
@@ -10,12 +10,87 @@ import GameOver from "./components/GameOver";
 import { useState, useEffect } from "react";
 
 function App() {
+    function shuffleArr(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    }
+
+    function randNum(range) {
+        return Math.floor(Math.random() * range);
+    }
+    function calc() {
+        let rand1 = randNum(14);
+        let rand2 = randNum(12);
+        let total = rand1 + rand2;
+        return `${rand1} + ${rand2} = ${total}`;
+    }
+
+    function fakeCalc() {
+        let rand1 = randNum(10);
+        let rand2 = randNum(10);
+        let rand3 = randNum(6);
+        let total = rand1 + rand3 + rand2 + 1;
+        return `${rand1} + ${rand2} = ${total}`;
+    }
+
+    let calcsArr = [
+        { ques: calc(), correct: true },
+        { ques: fakeCalc(), correct: false },
+        { ques: fakeCalc(), correct: false },
+        { ques: fakeCalc(), correct: false }
+    ];
+
+    let newCalcsArr = shuffleArr(calcsArr);
+    let colors = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet"
+    ];
+
+    let [newColors, setNewColors] = useState(shuffleArr(colors));
     let [fillWidth, setFillWidth] = useState(25);
+
+    function checkCalc(data) {
+        let newColor = shuffleArr(colors);
+        setNewColors(newColor);
+
+        if (data) {
+            setFillWidth(previousFillWith => previousFillWith + 75);
+        } else {
+            setFillWidth(previousFillWith => previousFillWith - 25);
+        }
+    }
+
+    
+        useEffect(() => {
+            if (fillWidth >= 100) {
+                alert("you win");
+                setFillWidth(100);
+            } else if (fillWidth <= 0) {
+                alert("you loss");
+                setFillWidth(0);
+            }
+        }, [fillWidth]);
+    
+
+    
+
     return (
-        <div className="h-[100dvh] flex flex-col bg-neutral-50}">
+        <div className="h-[100vh] flex flex-col bg-neutral-50">
             <Header />
             <Fill fill={fillWidth} />
-            <GameBoard setFill={setFillWidth} fill={fillWidth} />
+            <GameBoard
+                newColors={newColors}
+                newCalcsArr={newCalcsArr}
+                checkCalc={checkCalc}
+            />
             <ColorPalateView />
 
             <GameOver />
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,93 +1,36 @@
 import { useState } from "react";
-import Block from "./Color-Block.jsx";
+import Block from "./ColorBlock.jsx";
 
 function GameBoard(props) {
-    function shuffleArr(arr) {
-        for (let i = arr.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-        return arr;
-    }
-    function randNum(range) {
-        return Math.floor(Math.random() * range);
-    }
-    function calc() {
-        let rand1 = randNum(14);
-        let rand2 = randNum(12);
-        let total = rand1 + rand2;
-        return `${rand1} + ${rand2} = ${total}`;
-    }
-
-    function fakeCalc() {
-        let rand1 = randNum(10);
-        let rand2 = randNum(10);
-        let rand3 = randNum(6);
-        let total = rand1 + rand3 + rand2 + 1;
-        return `${rand1} + ${rand2} = ${total}`;
-    }
-
-    let calcsArr = [
-        { ques: calc(), correct: true },
-        { ques: fakeCalc(), correct: false },
-        { ques: fakeCalc(), correct: false },
-        { ques: fakeCalc(), correct: false }
-    ];
-
-    let newCalcsArr = shuffleArr(calcsArr);
-
-    let colors = [
-        "red",
-        "orange",
-        "yellow",
-        "green",
-        "blue",
-        "indigo",
-        "violet"
-    ];
-
-    let [newColors, setNewColors] = useState(shuffleArr(colors));
-    function checkCalc(data) {
-        let newColor = shuffleArr(colors);
-        setNewColors(newColor);
-        let fill;
-        if (data) {
-            fill = props.fill + 25;
-        } else {
-            fill = props.fill - 25;
-        }
-        props.setFill(fill);
-        if (fill >= 100) {
-            alert("you win");
-            props.setFill(100);
-        } else if (fill <= 0) {
-            alert("you loss");
-            props.setFill(0);
-        } else {
-            return;
-        }
-    }
+  let newColors = props.newColors
+  let newCalcsArr = props.newCalcsArr
+  let checkCalc = props.checkCalc
+  
     return (
         <div className=" h-[100dvw] w-[100dvw] grid grid-cols-2 grid-rows-2 gap-1 pl-1 pr-1">
             <Block
-                color={newColors[0]}
-                text={newCalcsArr[0].ques}
-                handleClick={() => checkCalc(newCalcsArr[0].correct)}
+                color={props.newColors[0]}
+                text={props.newCalcsArr[0].ques}
+                handleClick={() =>
+                    props.checkCalc(props.newCalcsArr[0].correct)
+                }
             />
             <Block
-                color={newColors[1]}
-                text={newCalcsArr[1].ques}
-                handleClick={() => checkCalc(newCalcsArr[1].correct)}
+                color={props.newColors[1]}
+                text={props.newCalcsArr[1].ques}
+                handleClick={() =>
+                    props.checkCalc(props.newCalcsArr[1].correct)
+                }
             />
             <Block
-                color={newColors[2]}
-                text={newCalcsArr[2].ques}
-                handleClick={() => checkCalc(newCalcsArr[2].correct)}
+                color={props.newColors[2]}
+                text={props.newCalcsArr[2].ques}
+                handleClick={() => props.checkCalc(props.newCalcsArr[2].correct)}
             />
             <Block
-                color={newColors[3]}
-                text={newCalcsArr[3].ques}
-                handleClick={() => checkCalc(newCalcsArr[3].correct)}
+                color={props.newColors[3]}
+                text={props.newCalcsArr[3].ques}
+                handleClick={() => props.checkCalc(props.newCalcsArr[3].correct)}
             />
         </div>
     );
